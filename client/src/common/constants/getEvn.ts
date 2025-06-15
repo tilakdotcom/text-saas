d7d08540a0661a660a5374d88b46038eb76e0dab
tilak-dev <tsingh99991@gmail.com>
2025-06-15 10:27:26 +0530

    env error fixed

--- client/src/common/constants/getEvn.ts
+++ client/src/common/constants/getEvn.ts
@@ -1,15 +1,3 @@
-export const getEnv = (key: string, dafaultKey?: string): string => {
-  const env = process.env[key as keyof typeof process.env] || dafaultKey;
-
-  if (!env) {
-    throw new Error(`Environment variable ${key} is not defined.`);
-  }
-
-  return env;
-};
-
-export const CLIENT_URI = getEnv("CLIENT_URI", "http://localhost:3000");
-
-export const BACKEND_URI = getEnv("BACKEND_URI", "http://localhost:5000/api/v1");
-
-export const CLIENT_ID = getEnv("CLIENT_ID", "sgsf");
+export const CLIENT_URI = process.env.NEXT_PUBLIC_CLIENT_URI;
+export const BACKEND_URI = process.env.NEXT_PUBLIC_BACKEND_URI;
+export const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID;
